38862f5b6e19e7bcc8bb9c79f90f174d8fe0eeb6
carolinerosa.nave@gmail.com <carolinerosa.nave@gmail.com@d36eccf1-153d-4623-3f2a-c534f000dca0>
2013-04-22 21:59:45 +0000

    Inicio da implementação dos recursos

--- src/example/maocorda/Game.java
+++ src/example/maocorda/Game.java
@@ -19,6 +19,11 @@ public class Game extends View implements Runnable
 	Boolean possivel =false;
 	static Rect atual=new Rect();
 	static Rect corda=new Rect();
+	static Rect Impulso=new Rect();
+
+	private int counter;
+	private int period=100;
+	private int current;
 	Paint paint = new Paint();
 	static float positionX=40;
 	private static float positionY=30;
@@ -26,6 +31,10 @@ public class Game extends View implements Runnable
 	private static float Height=30;
 	ImageManager img;
 	Bitmap imagem;
+	Bitmap impulso;
+	int Forca;
+	int Num_impulso;
+	Boolean impp = false;
 	
 	public Game(Context context) {
 		super(context);
@@ -37,6 +46,7 @@ public class Game extends View implements Runnable
 		paint.setTextSize(20); 
 		img = new ImageManager((int)positionY*2,(int)positionY*2);
 		imagem=img.ImageManager("Game_Guerra.jpg", context);
+		impulso=img.ImageManager("amarelo.png", context);
 		
 		Thread processo = new Thread(this);
 		processo.start();
@@ -46,11 +56,13 @@ public class Game extends View implements Runnable
 	protected void onSizeChanged(int w, int h, int oldw, int oldh) 
 	{
 		super.onSizeChanged(w, h, oldw, oldh);
+		Impulso.set((int)getWidth()/10,(int)6*getHeight()/10 , (int)3*getWidth()/10, (int)8*getHeight()/10);
 		
 		Width= getWidth()/2;
 		Height=getHeight()/2;
 		positionX=Width;
 		positionY=Height;
+		
 	
 	}
 	
@@ -61,9 +73,15 @@ public class Game extends View implements Runnable
 			Log.i("foi", "down baby down !! ");
 			q = (int)event.getRawX();
 			r = (int)event.getRawY();
-			if(corda.contains(q, r)){
+			
+			if(Impulso.contains(q, r- Impulso.height())){
+				impp=true;
+				Num_impulso++;
+				current= period;
+			}
+			if(corda.contains(q, r-corda.height()/2)){
 			possivel=true;
-			//aplicarForca(10);
+			
 
 			}
 			else{
@@ -71,8 +89,18 @@ public class Game extends View implements Runnable
 			}
 		}
 		
-
+		if (event.getAction() == MotionEvent.ACTION_MOVE) 
+		{
 		
+			if(possivel && q<event.getRawX()){
+				positionX = event.getRawX();
+			}
+			else{
+				possivel=false;
+			}
+		
+
+		}
 		if (event.getAction() == MotionEvent.ACTION_UP) 
 		{
 			Log.i("foi", "UP !!!");
@@ -80,23 +108,34 @@ public class Game extends View implements Runnable
 			int b = (int)event.getRawY();
 			
 			if(possivel){
-				if(a-q >=4){
 				
-					aplicarForca((int)(a-q)/3);
+				if(a-q >=4){
+					
+					aplicarForca((int)(a-q)/3*Num_impulso);
+					possivel=false;
+					
 				}
 				else{
 					possivel=false;
+					impp=false;
+
 				}
 			
 			}
+			if(impp){
+				Num_impulso= (current -period)*10;
+				impp=false;
+			}
+			positionX=Width;
 			
 		}
 		return super.onTouchEvent(event);
 	}
 		private void aplicarForca(int i) {
 				
-				positionX+=i;
+				//positionX+=i;
 				
+			Forca =i;
 				// TODO Auto-generated method stub
 				
 			}
@@ -106,11 +145,13 @@ public class Game extends View implements Runnable
 		super.draw(canvas);
 		
 		atual.set(0, 0, (int)Width*2, (int)Height*2);
-		corda.set((int)positionX,(int) positionY,(int) positionX+20,(int) positionY+10);
-		
+		corda.set((int)positionX,(int) positionY,(int) positionX+200,(int) positionY+100);
 		canvas.drawBitmap(imagem, null, atual, paint);
 		canvas.drawRect(corda, paint);
+		canvas.drawText("forcaa"+Forca+"impulsooo"+Num_impulso, 50, 30, paint);
+		canvas.drawRect(Impulso, paint);
 
+		
 			
 	}
 	@Override
@@ -127,15 +168,28 @@ public class Game extends View implements Runnable
 			}
 			
 		
+			update();
 			postInvalidate();
 		}
 		
 		// TODO Auto-generated method stub
 		
+
 	}
 	
 	
-	
+	public void update(){
+		if (period !=0)
+		{
+			counter ++;
+		}
+		
+		if (counter == 1000)
+		{
+			period-=1;
+			counter = 0;
+		}
+	}
 
 
 
